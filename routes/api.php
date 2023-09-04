eb95dd552530a0f94709224cd469a28fa1918ab1
Steve McDougall <juststevemcd@gmail.com>
2023-09-04 16:02:54 +0100

    Starting to flesh out main routes

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,28 @@ declare(strict_types=1);
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Treblle\ApiResponses\Responses\MessageResponse;
 
-Route::middleware('auth:sanctum')->get(
-    'user',
-    fn (Request $request) => $request->user()
+Route::get('/', static fn (): MessageResponse => new MessageResponse(
+    data: 'Service Online',
+));
+
+Route::prefix('apps')->as('apps:')->group(
+    base_path('routes/apps.php'),
+);
+
+Route::prefix('auth')->as('auth:')->group(
+    base_path('routes/auth.php'),
+);
+
+Route::prefix('devices')->as('devices:')->group(
+    base_path('routes/devices.php'),
+);
+
+Route::prefix('packages')->as('packages:')->group(
+    base_path('routes/packages.php'),
+);
+
+Route::prefix('auth')->as('shells:')->group(
+    base_path('routes/shells.php'),
 );
---
+++ routes/packages.php
@@ -0,0 +1,7 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Support\Facades\Route;
+
+// @todo Packages Routes
